e773debe2c95716464b3cc0b90b1d646745ca3aa
beegeiger <beegeiger@gmail.com>
2020-07-21 14:22:49 -0700

    Working on debugging

--- tictac3.py
+++ tictac3.py
@@ -3,6 +3,7 @@ from time import sleep
 # Tic Tac Tow Game 3x3
 
 offical_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+board_copy = []
 game_status = "Unfinished"
 
 def start_game():
@@ -12,16 +13,19 @@ def start_game():
     print("Hi! Welcome to DB Geiger's new tic tac toe game!")
     input("Press Enter to continue...")
     while game_status == "Unfinished":
+        print("JUST BEFORE USER TURN", offical_board, game_status)
         user_turn()
         st = check_state(offical_board)
         if st != "Unfinished":
             game_status = st
         sleep(1)
+        print("JUST BEFORE COMPUTER TURN", offical_board, game_status)
         computer_turn()
         st = check_state(offical_board)
         if st != "Unfinished":
             game_status = st
         sleep(1)
+    print("BROKE WHILE LOOP FOR UNFINISHED GAME STATUS", offical_board, game_status)
     if game_status == "X win":
         print("Congradulations Human, You have bested me!")
     elif game_status == "O win":
@@ -38,12 +42,14 @@ def start_game():
     return
 
 def print_board():
-    print official_board[0:3]
-    print official_board[3:6]
-    print official_board[6:9]
+    print("SHOULD BE THE OFFICAL BOARD", offical_board, game_status)
+    print(offical_board[0:3])
+    print(offical_board[3:6])
+    print(offical_board[6:9])
 
 
 def user_turn():
+    print("USER TURN 1", offical_board, game_status)
     print("It is your turn, human!")
     print("Enter a number 1-9 available on the board.")
     print_board()
@@ -53,23 +59,29 @@ def user_turn():
         if len(val) > 1 or val.isdigit() == False:
             print("That was not a valid entry. Please enter a number 1-9 available on the board.")
             val = input()
-        elif official_board[val] == "X" or official_board[val] == "O":
+        elif offical_board[int(val)] == "X" or offical_board[int(val)] == "O":
             print("That square has already been taken. Please enter a number 1-9 STILL OPEN on the board.")
             val = input()
         else:
             entry_val = True
-    official_board[val] = "X"
+    offical_board[int(val) - 1] = "X"
+    print("USER TURN 2", offical_board, game_status)
     print_board()
     return 
 
 def computer_turn():
+    print("COMPUTER TURN 1", offical_board, game_status)
     opts = check_options(offical_board)
+    print("COMPUTER TURN 2", offical_board, game_status)
     for y in range(9):
         if offical_board[y].isalpha():
             opts[y] = -1000
+    print("COMPUTER TURN 3", offical_board, game_status)
     highest = max(opts)
+    print("COMPUTER TURN 4", offical_board, game_status)
     loc = opts.index(highest)
     offical_board[loc] = "O"
+    print("COMPUTER TURN 5", offical_board, game_status)
     print("Here is my response to your move!")
     print_board()
     return
@@ -86,11 +98,13 @@ def check_state(board):
     return "Unfinished"
 
 def check_options(board):
+    print("CHECK_OPTIONS 1", offical_board, game_status)
     board_copy = []
     chances = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     for y in range(9):
         board_copy = board
-        if board_copy[y].isdigit():
+        print("CHECK_OPTIONS 2",board_copy, offical_board, game_status)
+        if str(board_copy[y]).isdigit():
             board_copy[y] = "O"
             out = check_state(board_copy)
             if out == "O win":
@@ -111,3 +125,4 @@ def check_options(board):
     return chances
 
 
+start_game()
