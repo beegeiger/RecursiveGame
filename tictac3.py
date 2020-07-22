3b5222b4a61a9afea828201c39cfff751329bcea
beegeiger <beegeiger@gmail.com>
2020-07-21 17:49:10 -0700

    Debugging. Having issues with Check Options continually adding X's

--- tictac3.py
+++ tictac3.py
@@ -1,14 +1,12 @@
 from time import sleep
 
 # Tic Tac Tow Game 3x3
-
-offical_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 board_copy = []
+offical_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 game_status = "Unfinished"
 
 def start_game():
     #Function to run game
-    offical_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     game_status = "Unfinished"
     print("Hi! Welcome to DB Geiger's new tic tac toe game!")
     input("Press Enter to continue...")
@@ -59,7 +57,7 @@ def user_turn():
         if len(val) > 1 or val.isdigit() == False:
             print("That was not a valid entry. Please enter a number 1-9 available on the board.")
             val = input()
-        elif offical_board[int(val)] == "X" or offical_board[int(val)] == "O":
+        elif offical_board[int(val) - 1] == "X" or offical_board[int(val) - 1] == "O":
             print("That square has already been taken. Please enter a number 1-9 STILL OPEN on the board.")
             val = input()
         else:
@@ -72,10 +70,11 @@ def user_turn():
 def computer_turn():
     print("COMPUTER TURN 1", offical_board, game_status)
     opts = check_options(offical_board)
+    print("COMPUTER TURN OPTS FROM CHECK_OPTIONS OUTPUT: ", opts)
     print("COMPUTER TURN 2", offical_board, game_status)
     for y in range(9):
-        if offical_board[y].isalpha():
-            opts[y] = -1000
+        if str(offical_board[y]).isalpha():
+            opts[y] = -100000
     print("COMPUTER TURN 3", offical_board, game_status)
     highest = max(opts)
     print("COMPUTER TURN 4", offical_board, game_status)
@@ -87,13 +86,17 @@ def computer_turn():
     return
 
 def check_state(board):
+    print("OFFICAL BOARD: ", offical_board)
+    print("CHECK STATE BOARD", board, board.count("X"), board.count("O"))
     options = [board[0:3], board[3:6], board[6:9], [board[0], board[3], board[6]], [board[1], board[4], board[7]], [board[2], board[5], board[8]], [board[0], board[4], board[8]], [board[2], board[4], board[6]]]
     for option in options:
         if option.count("X") == 3:
             return "X win"
         elif option.count("O") == 3:
             return "O win"
+    print("CHECK STATE COUNT OF X AND O: ", board, board.count("X"), board.count("O"))
     if board.count("X") + board.count("O") == 9:
+        print("TIE!!!!!!!!!! IN CHECK STATE")
         return "Tie"
     return "Unfinished"
 
@@ -102,27 +105,46 @@ def check_options(board):
     board_copy = []
     chances = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     for y in range(9):
-        board_copy = board
-        print("CHECK_OPTIONS 2",board_copy, offical_board, game_status)
+        board_copy = list(board)
+        # print("CHECK_OPTIONS 2",board_copy, offical_board, game_status)
         if str(board_copy[y]).isdigit():
             board_copy[y] = "O"
             out = check_state(board_copy)
             if out == "O win":
+                print("O WIN!!!!!!!!!!")
                 chances[y] += 50
             elif out == "Unfinished":
                 for z in range(9):
-                    if board_copy[y].isdigit():
-                        board_copy[y] = "X"
+                    if str(board_copy[z]).isdigit():
+                        board_copy[z] = "X"
                         outx = check_state(board_copy)
                         if outx == "X win":
+                            print("X WIN!!!!!!!!!!")
                             chances[y] -= 50
                         elif outx == "Tie":
+                            print("TIE!!!!!!!!!!")
                             chances[y] += 1
-                        elif outx == "Y win":
+                        elif outx == "O win":
                             print("ERROR: THIS SHOULD NOT BE BEING CALLED FROM CHECK_OPTIONS")
                         else:
                             chances[y] += sum(check_options(board_copy))
+
+    print("CHECK_OPTIONS 3", chances)
     return chances
+
+# X O X
+# O O X
+# X 8 9
+
+# ["X", "O", "X", "O", "O", "X", "X", 8 ,9]
+# chances = [0, 0, 0, 0, 0, 0, 0, 50, 1]
+
+
+
+
+
+
+
 
 
 start_game()
